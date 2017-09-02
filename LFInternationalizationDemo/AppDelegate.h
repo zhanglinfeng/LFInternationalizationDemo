25df60db7a0e7b0186ebcae83b135b1ed6d74155
张林峰 <1051034428@qq.com>
2017-09-02 23:00:18 +0800

    Initial Commit

---
+++ LFInternationalizationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LFInternationalizationDemo
+//
+//  Created by 张林峰 on 2017/9/2.
+//  Copyright © 2017年 张林峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LFInternationalizationDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LFInternationalizationDemo
+//
+//  Created by 张林峰 on 2017/9/2.
+//  Copyright © 2017年 张林峰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
